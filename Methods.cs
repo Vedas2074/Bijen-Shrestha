cf18776860d5d8b983666f1e1e7d5b9fb9210366
BijenDShrestha <bijenshrestha09@gmail.com>
2020-09-29 10:02:52 +0545

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,72 @@
+using System
+using System.Linq
+
+
+class Methods
+{
+    void PrintInfo()
+    {
+        Console.Writeline("Printing something...");
+        Multiply(4.6f, 7.5f);
+        Sum(8.9,9.8);
+
+        Greet("Bijen", "Mr.");
+        Greet("shrestha");
+
+        byte[] sequence = {4, 3, 2, 5, 6, 89};
+        (byte Minimum, byte Maximum) result = FindMinMax(sequence);
+        Console.Wtireline(result.Minimum);
+
+        PrintCustomerDetails(age: 57, name: "Bijen" address: "Thimi");
+    }
+
+    void PrintSomething(string message)
+    {
+        Console.Writeline(message);
+    }
+
+    void Multiply(float firstNum, float secondNum)
+    {
+        float product = firstNum * secondNum;
+        return product;
+    }   
+    void Multiply(float firstNum, float secondNum, float thirdNum)
+    {
+        float product = firstNum * secondNum * thirdNum;
+        return product;
+    }
+
+    // variable arguments 
+    double Sum(params double[] numbers)
+    {
+        double sum = 0;
+        foreach (double num in numbers)
+        {
+            sum = sum + num;
+        }
+
+        return sum;
+    }
+
+    // optional arguments
+    string Greet(string name, string prefix = "Miss")
+    {
+        string fullName = $"{prefix}, {name}"; //string interpolation
+        //string fullName = prefix + "," + name;
+        return fullName;
+    }
+
+    //Returning Multiple values : using tuple
+    (byte, byte) FindMinMax(byte[] numbers)
+    {
+        byte max = numbers.Max();
+        byte min = numbers.MIN();
+
+        return (min, max);
+    }
+
+    void PrintCustomerDetails(string name, string address, byte age)
+    {
+        Console.Writeline($"{name}, {age}, {address}");
+    }
+}
